ad3cae142e4d1ee94e16a967a5373427390070dd
rhansonrizing <126808476+rhansonrizing@users.noreply.github.com>
2026-03-04 07:32:51 -0800

    Update oauthClientId in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   mapServiceUrl:    "https://rhapps-prod.dot.ca.gov/ars/rest/services/TSMIS/lrs_tsmis_prod/MapServer",
-  oauthClientId:    "z7jnDQesKUG5xAxv",
+  oauthClientId:    "wVe0TkJtLjYUttoJ",
   oauthAuthorizeUrl:"https://rhapps-prod.dot.ca.gov/portal/sharing/rest/oauth2/authorize",
   oauthTokenUrl:    "https://rhapps-prod.dot.ca.gov/portal/sharing/rest/oauth2/token",
   oauthRedirectUrl: "http://localhost:5500/index.html"
