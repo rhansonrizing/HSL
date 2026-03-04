5b3061030f7f23176b4ac5c64388dc9d98270c52
Ryan Hanson <ryan.hanson@rizing.io>
2026-03-04 09:23:16 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  mapServiceUrl:    "https://arcgis.rizing.dev/primary/rest/services/Caltrans/lrs_tsnr/MapServer",
+  oauthClientId:    "LdLDgsAFMZ7b0bk5",
+  oauthAuthorizeUrl:"https://arcgis.rizing.dev/portal/sharing/rest/oauth2/authorize",
+  oauthTokenUrl:    "https://arcgis.rizing.dev/portal/sharing/rest/oauth2/token",
+  oauthRedirectUrl: "http://localhost:5500/index.html"
+};
